ed914b68ebcd84637ec5ad1e082a30679d09c438
Atsushi-Noguchi <gunogunoatushi@gmail.com>
2023-11-18 10:22:36 +0900

    first-add

---
+++ src/main/java/com/example/testtest/controllers/testtest.java
@@ -0,0 +1,12 @@
+package com.example.testtest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testtest {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "test";
+    }
+}
